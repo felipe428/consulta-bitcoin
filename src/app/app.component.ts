41ffecb9e7ef825283b28d89cedaaedd75528e58
felipe428 <felipe.silva428@fatec.sp.gov.br>
2021-09-17 20:49:59 -0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component, VERSION } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular ' + VERSION.major;
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(){
+
+  }
+
+  updateGithubRepos(){
+
+  }
+}
